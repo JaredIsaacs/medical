5f9b0e8c9f2312f546e627cd32357602c9ad2684
JaredIsaacs <jaredisaacs6@gmail.com>
2024-09-28 18:41:34 -0400

    icons :)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-advisor_page=st.Page("pages/advisor.py", title="Advisor")
-portfolio_page=st.Page("pages/portfolio.py",title="Portfolio")
-companies_page=st.Page("pages/companies.py",title="Companies")
+
+advisor_page=st.Page("pages/advisor.py", title="Advisor", icon=":material/person:")
+portfolio_page=st.Page("pages/portfolio.py", title="Portfolio", icon=":material/account_balance:")
+companies_page=st.Page("pages/companies.py", title="Companies", icon=":material/add_to_queue:")
+
 pg = st.navigation([advisor_page,portfolio_page,companies_page])
 pg.run()
