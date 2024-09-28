5cc17d54a72f1565ea457463781098818638c2d3
Johnny Casares <88862676+JohnnyCasares@users.noreply.github.com>
2024-09-28 19:25:13 -0400

    login fixed. hide nav bar. improved file structure. added init py and toml files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,40 @@
 import streamlit as st
-advisor_page=st.Page("pages/advisor.py", title="Advisor")
-portfolio_page=st.Page("pages/portfolio.py",title="Portfolio")
-companies_page=st.Page("pages/companies.py",title="Companies")
-pg = st.navigation([advisor_page,portfolio_page,companies_page])
-pg.run()
+
+from pages import login
+
+# run login
+
+x = login.joniFunc()
+if x:
+    # User is logged in, show the sidebar
+    st.markdown(
+        """
+        <style>
+        div[data-testid="stSidebar"] {
+            display: block; 
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+else:
+    # User is not logged in, hide the sidebar
+    st.markdown(
+        """
+        <style>
+        div[data-testid="stSidebar"] {
+            display: none;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+if x:
+    advisor_page = st.Page("pages/advisor.py", title="Advisor")
+    portfolio_page = st.Page("pages/portfolio.py", title="Portfolio")
+    companies_page = st.Page("pages/companies.py", title="Companies")
+    sign_out = st.Page("pages/logout.py",title='Sign Out')
+    pg = st.navigation([advisor_page, portfolio_page, companies_page,])
+    pg.run()
+
