e6f961f7b1f0028e43ea0cf39edfbb845149973a
JaredIsaacs <jaredisaacs6@gmail.com>
2024-09-28 19:31:59 -0400

    fixed merge issues

--- app/main.py
+++ app/main.py
@@ -39,10 +39,3 @@ if x:
     pg = st.navigation([advisor_page, portfolio_page, companies_page,])
     pg.run()
 
-=======
-advisor_page=st.Page("pages/advisor.py", title="Advisor", icon=":material/person:")
-portfolio_page=st.Page("pages/portfolio.py", title="Portfolio", icon=":material/account_balance:")
-companies_page=st.Page("pages/companies.py", title="Companies", icon=":material/add_to_queue:")
-
-pg = st.navigation([advisor_page,portfolio_page,companies_page])
-pg.run()
